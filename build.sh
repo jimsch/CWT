bda07de477cee5eae3adc5be4227588622dec715
Jim Schaad <ietf@augustcellars.com>
2017-10-14 13:56:27 -0700

    Update the version and debug the packages

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 set -ev
 mono nuget.exe restore $SLN
 
+ls packages
+ls packages/Portable.BouncyCastle.1.8.1.3
+ls packages/Portable.BouncyCastle.1.8.1.3/lib
+
+
 msbuild /p:Configuration=$VERSION $SLN
 
 mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./WebTokenTest/bin/$VERSION/WebTokenTest.dll
